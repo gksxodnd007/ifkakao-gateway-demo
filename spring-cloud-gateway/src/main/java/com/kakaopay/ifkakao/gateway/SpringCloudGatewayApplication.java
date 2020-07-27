e7805a197a6ba1944163cf2bcbd7feb3e62b6d19
hubert.squid <hubert.squid@kakaopaycorp.com>
2020-07-27 23:37:53 +0900

    [Feature] package명 변경

--- spring-cloud-gateway/src/main/java/com/kakaopay/ifkakao/gateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/kakaopay/ifkakao/gateway/SpringCloudGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.kakaopay.ifkakao;
+package com.kakaopay.ifkakao.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
